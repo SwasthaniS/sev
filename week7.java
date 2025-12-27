571d947b6a25de17fbbcb18f9d10f6e29ceb8a87
SwasthaniS <swasthanistha@gmail.com>
2025-12-27 23:15:34 +0545

    caller

--- week7.java
+++ week7.java
@@ -8,7 +8,7 @@ class mainBranch{
 	public void login() {
 		System.out.println("Contains login branch");
 	}
-	public void ut() {
-		System.out.println("Contains signout");
+	public int ut(int a , int b) {
+		return 8;
 	}
 }
